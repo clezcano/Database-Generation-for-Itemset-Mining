ae32c0921ac8d2a374967bf91c2661d4ad8bdf8f
Christian Lezcano <clezcanopy@gmail.com>
2016-05-23 21:17:42 +0200

    end of the day1

--- DBGen-main.py
+++ DBGen-main.py
@@ -1,3 +1,5 @@
 with open(r'C:\Users\SF\Desktop\groceries.csv', 'r') as file:
     #print(file.read())
-    print(len(file.readlines()))
+    #print(len(file.readlines()))
+    print((file.readlines()))
+    hola
