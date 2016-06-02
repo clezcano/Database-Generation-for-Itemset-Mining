eb08f8e00095a104b35217884e9f02d65efc700a
clezcano <clezcanopy@gmail.com>
2016-06-02 12:56:18 +0200

    reading text file

--- DBGen-main.py
+++ DBGen-main.py
@@ -1,5 +1,11 @@
-with open(r'C:\Users\SF\Desktop\groceries.csv', 'r') as file:
-    #print(file.read())
-    #print(len(file.readlines()))
-    print((file.readlines()))
-#dfsdfdsfhola
+import csv
+filein = 'groceries.csv'
+#list = []
+with open(filein, 'r') as f:
+    w = csv.reader(f, delimiter=',')
+    rownum = 0
+    for i, row in enumerate(w, start=1):
+        print("#",i, ": ", end='')
+        for col in row:
+            print(col, end=' , ')
+        print ('\n')
