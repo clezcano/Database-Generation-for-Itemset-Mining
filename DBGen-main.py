f67a2b0924400311d8813f45de46a14561672312
Christian Lezcano <clezcanopy@gmail.com>
2016-06-05 19:31:35 +0200

    testing more desktop

--- DBGen-main.py
+++ DBGen-main.py
@@ -1,19 +1,21 @@
-from subprocess import call
 
-algorithm = "apriori.exe"
-minsup = "-s10" # value in percentage
-itemsepin = "-f," # input item separator
-itemsepout = "-k," # output item separator
-targetype = "-ts" # frequest (s) maximal (m) closed (c)
-apriorifile = "groceries.csv"
-frequentout = "frequentout.csv"
-maximalout = "maximalout.csv"
+if __name__ == "__main__":
 
-cmd = algorithm + " " + itemsepin + " " + minsup + " " + itemsepout + " " + targetype + " " + apriorifile + " " + frequentout
-print("command: ", cmd)
-call(cmd, shell=True)
+        from subprocess import call
+        algorithm = "apriori.exe"
+        itemsepin = "-f," # input item separator
+        itemsepout = "-k," # output item separator
+        minsup = "-s25" # minimun support, positive in percentage, negative in absolutes
+        targetype = "-ts" # frequest (s) maximal (m) closed (c)
+        apriorifile = "groceries.csv"
+        frequentout = "frequentout.csv"
+        maximalout = "maximalout.csv"
 
-maximal = "-tm"
-cmd = algorithm + " " + itemsepin + " " + minsup + " " + itemsepout + " " + targetype + " " + apriorifile + " " + maximalout
-print("command: ", cmd)
-call(cmd, shell=True)
+        cmd = algorithm + " " + itemsepin + " " + itemsepout + " " + minsup + " " + targetype + " " + apriorifile + " " + frequentout
+        print("command: ", cmd)
+        call(cmd, shell=True)
+
+        targetype = "-tm"
+        cmd = algorithm + " " + itemsepin + " " + itemsepout + " " + minsup + " " + targetype + " " + apriorifile + " " + maximalout
+        print("command: ", cmd)
+        call(cmd, shell=True)
