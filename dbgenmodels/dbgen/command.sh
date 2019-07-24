8a61045b579468c2f37a4ed0dff03e330c9c49f7
Christian Lezcano <clezcanopy@gmail.com>
2019-07-24 05:04:33 +0200

    d

--- dbgenmodels/dbgen/command.sh
+++
@@ -1,5 +0,0 @@
-./eclat -f"," -s-50 -k"," -Z groceries.csv output-orig
-./eclat -f"," -s-50 -k"," -Z x_groceries.csv output-gen
-
-
-java -cp exe/itemset-mining-1.0.jar itemsetmining.main.ItemsetMining -i 100 -f example.dat -v 
